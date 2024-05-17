7274d3287adb3b89e115051f79d6594861827e65
Fabian Ramirez <fabianramirez0072@gmail.com>
2024-05-17 09:15:56 -0500

    upgrade project

--- Message.Api/Program.cs
+++ Message.Api/Program.cs
@@ -1,23 +1,14 @@
-using Message.Application.UseCase.HandlersEvents;
 using Message.Application.UseCase.DomainEvents;
-using Message.Infrastructure.Extensions;
-using Message.Infrastrunture;
-using MediatR;
 using Common.Communication.Consumer.Manager;
 using Common.Communication.Messages;
+using Message.Infrastrunture;
+using MediatR;
 
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddInfrastructure(builder.Configuration);
-
-builder.Services.AddServiceBusIntegrationConsumer(builder.Configuration);
-builder.Services.AddHandlersInAssembly<EmailHandler>();
-
 var app = builder.Build();
-
-
 app.UseInfrastructure(app.Environment);
-
 
 app.MapPost("/Send", async (EmailCreateCommand emailDto) =>
 {
@@ -34,8 +25,6 @@ app.MapPut("/Start", (IConsumerManager<IntegrationMessage> consumerManager) =>
 })
 .WithName("Integration")
 .WithOpenApi();
-
-
 
 app.Run();
 public partial class Program { }
--- Message.Infrastructure/Startup.cs
+++ Message.Infrastructure/Startup.cs
@@ -1,13 +1,9 @@
-﻿using Hangfire;
-using Common.Serialization;
+﻿using Common.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Common.Communication.Messages;
 using Message.Infrastructure.Extensions;
 using Microsoft.Extensions.Configuration;
-using Common.Communication.Consumer.Manager;
 using Microsoft.Extensions.DependencyInjection;
-using Common.Communication.RabbitMQ.Consumer;
 
 namespace Message.Infrastrunture;
 
@@ -22,26 +18,13 @@ public static class Startup
         services.AddEndpointsApiExplorer();
         services.AddSettingdServices(config);
         services.AddSerializer();
-        //services.AddHangfire(config);
+        services.AddServiceBusIntegrationConsumer(config);
     }
 
     public static void UseInfrastructure(this IApplicationBuilder app, IWebHostEnvironment env)
     {
-
-
-        app.ApplicationServices.GetRequiredService<IMessageConsumer<IntegrationMessage>>();
-        //IBackgroundJobClient backgroundJobs = app.ApplicationServices.GetRequiredService<IBackgroundJobClient>();
-        
-
-        //app.UseHangfireDashboard();
-        //app.UseHangfireDashboard("/dashboard");
-        
-        //RecurringJob.AddOrUpdate("testconsole", () => Console.WriteLine("Simple!"), "* * * * 5 ");
-        //RecurringJob.AddOrUpdate<RabbitMQMessageConsumer<IntegrationMessage>>("sender email", x => x.StartAsync(default), "*/5 * * * *");
         app.UseSwaggers(env);
         app.UseExceptionMiddleware();
         app.UseHttpsRedirection();
     }
-
-    
 }
